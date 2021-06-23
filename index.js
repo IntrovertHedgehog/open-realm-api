68209a116a2be0aa0321b2a149a1929bda4e4219
Laridian <larby2409@gmail.com>
2021-06-23 18:13:23 +0800

    allow frontend

--- index.js
+++ index.js
@@ -15,29 +15,29 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 
 var corsOptions = {
-  origin: isProduction ? "https://openrealmfront.herokuapp.com/" : "*",
+  origin: "https://openrealmfront.herokuapp.com/",
 };
 
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://openrealmfront.herokuapp.com/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// app.use(function (req, res, next) {
+//
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://openrealmfront.herokuapp.com/');
+//
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 // comprpess http
 app.use(compression());
